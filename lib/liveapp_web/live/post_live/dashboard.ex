d9e93ddecc0ff5ba9ad3d5c6c10a142187b675b0
Your Name <you@example.com>
2023-04-09 13:27:37 -0700

    updated posts

--- lib/liveapp_web/live/post_live/dashboard.ex
+++ lib/liveapp_web/live/post_live/dashboard.ex
@@ -8,26 +8,32 @@ defmodule LiveappWeb.PostLive.Dashboard do
   def mount(_params, _session, socket) do
     # IO.inspect(socket, limit: :infinity, structs: false)
     IO.inspect(socket, pretty: true, label: "Socket in MOUNT")
+    # {:ok, IO.inspect("stuff")}
     {:ok, stream(socket, :posts, Timeline.list_posts())}
   end
 
   @impl true
   def handle_params(params, _url, socket) do
     IO.inspect(params, pretty: true, label: "Params in handle params")
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+    {:noreply,
+      socket
+      |> assign(:posts, length(Timeline.list_posts()))}
+    # {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Post")
-    |> assign(:post, %Post{})
-  end
+  # def apply_action(socket, )
 
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Posts")
-    |> assign(:post, nil)
-  end
+  # defp apply_action(socket, :new, _params) do
+  #   socket
+  #   |> assign(:page_title, "New Post")
+  #   |> assign(:post, %Post{})
+  # end
+
+  # defp apply_action(socket, :index, _params) do
+  #   socket
+  #   |> assign(:page_title, "Listing Posts")
+  #   |> assign(:post, nil)
+  # end
 
   @impl true
   def handle_info({LiveappWeb.PostLive.FormComponent, {:saved, post}}, socket) do
